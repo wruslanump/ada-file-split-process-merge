bb3e75e2b1ab01b4127de99141e2dc9410d63165
wruslandr <wruslandr@gmail.com>
2021-03-18 02:41:07 +0800

    initial commit

---
+++ src/main_ada_file_split_process_merge.adb
@@ -0,0 +1,49 @@
+-- File   : main_ada_file_split_process_merge.adb
+-- Date   : Fri 12 Mar 2021 07:24:15 AM +08
+-- Author : wruslandr@gmail.com
+-- Version: 1.0 Sun 07 Mar 2021 06:00:42 PM +08
+-- ========================================================
+
+-- IMPORT STANDARD ADA PACKAGES
+with Ada.Text_IO;
+use  Ada.Text_IO;
+
+
+-- IMPORT USER-DEFINED ADA PACKAGES
+
+
+-- ========================================================
+procedure main_ada_file_split_process_merge
+-- ========================================================
+--	with SPARK_Mode => on
+is 
+   -- RENAME STANDARD ADA PACKAGES FOR CONVENIENCE
+   package ATIO   renames Ada.Text_IO;
+   
+      
+   -- RENAME USER-DEFINED ADA PACKAGES FOR CONVENIENCE
+   
+      
+   -- PACKAGE-WIDE VARIABLE DEFINITIONS
+   -- ====================================================
+   
+
+-- ========================================================   
+begin  -- FOR procedure main_xxx
+   
+   ATIO.Put_Line ("STARTED: main Bismillah 3 times WRY");
+   ATIO.New_Line;
+   
+   -- CODE BEGINS HERE
+   -- =====================================================
+   
+   
+   
+   -- CODE ENDS HERE
+   -- =====================================================
+   
+   ATIO.New_Line;
+   ATIO.Put_Line ("ENDED: main Alhamdulillah 3 times WRY");
+-- ========================================================   
+end main_ada_file_split_process_merge;
+-- ========================================================
